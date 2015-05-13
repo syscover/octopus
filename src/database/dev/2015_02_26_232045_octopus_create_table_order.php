1ea33e7fa07a52869e08b15d6aa3dccc4788b19e
Jose Carlos <cpalacin@syscover.com>
2015-05-13 23:15:39 +0200

    database

--- src/database/dev/2015_02_26_232045_octopus_create_table_order.php
+++ src/database/dev/2015_02_26_232045_octopus_create_table_order.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class OctopusCreateTableEntregado extends Migration {
+class OctopusCreateTableOrder extends Migration {
 
 	/**
 	 * Run the migrations.
--- src/database/dev/2015_02_26_232106_octopus_create_table_committed.php
+++ src/database/dev/2015_02_26_232106_octopus_create_table_committed.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class OctopusCreateTablePedido extends Migration {
+class OctopusCreateTableCommitted extends Migration {
 
 	/**
 	 * Run the migrations.
