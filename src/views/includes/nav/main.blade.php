9b2564731915b02d04311096d35393ebf90cd115
Jose Carlos <cpalacin@syscover.com>
2015-09-01 19:12:56 +0200

    font awesome

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -2,13 +2,13 @@
             <a href="javascript:void(0);"><i class="sys-icon-octopus"></i>Octopus</a>
             <ul class="sub-menu">
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'mifinan', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('mifinan') !!}><a href="{{ route('OctopusFamily') }}"><i class="icon-th-large"></i>{{ trans_choice('octopus::pulsar.committed', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('mifinan') !!}><a href="{{ route('OctopusFamily') }}"><i class="fa fa-th-large"></i>{{ trans_choice('octopus::pulsar.committed', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'mifinan', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('octopus-order') !!}><a href="{{ route('OctopusOrder') }}"><i class="icon-refresh"></i>{{ trans_choice('octopus::pulsar.order', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('octopus-order') !!}><a href="{{ route('OctopusOrder') }}"><i class="fa fa-refresh"></i>{{ trans_choice('octopus::pulsar.order', 2) }}</a></li>
                 @endif
                 @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'octopus-request', 'access'))
-                    <li{!! Miscellaneous::setCurrentPage('octopus-request') !!}><a href="{{ route('OctopusRequest') }}"><i class="icon-inbox"></i>{{ trans_choice('octopus::pulsar.request', 2) }}</a></li>
+                    <li{!! Miscellaneous::setCurrentPage('octopus-request') !!}><a href="{{ route('OctopusRequest') }}"><i class="fa fa-inbox"></i>{{ trans_choice('octopus::pulsar.request', 2) }}</a></li>
                 @endif
                 <li{!! Miscellaneous::setCurrentOpenPage(['octopus-family','octopus-brand','octopus-product','octopus-laboratory','octopus-company','octopus-customer','octopus-shop', 'octopus-preference']) !!}>
                     <a href="javascript:void(0);"><i class="icomoon-icon-grid"></i>{{ trans('pulsar::pulsar.master_tables') }}</a>
@@ -20,7 +20,7 @@
                             <li{!! Miscellaneous::setCurrentPage('octopus-customer') !!}><a href="{{ route('OctopusCustomer') }}"><i class="icomoon-icon-users"></i>{{ trans_choice('pulsar::pulsar.customer', 2) }}</a></li>
                         @endif
                         @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'octopus-company', 'access'))
-                            <li{!! Miscellaneous::setCurrentPage('octopus-company') !!}><a href="{{ route('OctopusCompany') }}"><i class="icon-building"></i>{{ trans_choice('octopus::pulsar.company', 2) }}</a></li>
+                            <li{!! Miscellaneous::setCurrentPage('octopus-company') !!}><a href="{{ route('OctopusCompany') }}"><i class="fa fa-building"></i>{{ trans_choice('octopus::pulsar.company', 2) }}</a></li>
                         @endif
                         @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'octopus-laboratory', 'access'))
                             <li{!! Miscellaneous::setCurrentPage('octopus-laboratory') !!}><a href="{{ route('OctopusLaboratory') }}"><i class="icomoon-icon-lab"></i>{{ trans_choice('octopus::pulsar.laboratory', 2) }}</a></li>
@@ -32,10 +32,10 @@
                             <li{!! Miscellaneous::setCurrentPage('octopus-brand') !!}><a href="{{ route('OctopusBrand') }}"><i class="icomoon-icon-medal-2"></i>{{ trans_choice('octopus::pulsar.brand', 2) }}</a></li>
                         @endif
                         @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'octopus-family', 'access'))
-                            <li{!! Miscellaneous::setCurrentPage('octopus-family') !!}><a href="{{ route('OctopusFamily') }}"><i class="icon-align-justify"></i>{{ trans_choice('pulsar::pulsar.family', 2) }}</a></li>
+                            <li{!! Miscellaneous::setCurrentPage('octopus-family') !!}><a href="{{ route('OctopusFamily') }}"><i class="fa fa-align-justify"></i>{{ trans_choice('pulsar::pulsar.family', 2) }}</a></li>
                         @endif
                         @if(session('userAcl')->isAllowed(Auth::user()->profile_010, 'forms-preference', 'access'))
-                            <li{!! Miscellaneous::setCurrentPage('octopus-preference') !!}><a href="{{ route('OctopusPreference') }}"><i class="icon-cog"></i>{{ trans_choice('pulsar::pulsar.preference', 2) }}</a></li>
+                            <li{!! Miscellaneous::setCurrentPage('octopus-preference') !!}><a href="{{ route('OctopusPreference') }}"><i class="fa fa-cog"></i>{{ trans_choice('pulsar::pulsar.preference', 2) }}</a></li>
                         @endif
                     </ul>
                 </li>
