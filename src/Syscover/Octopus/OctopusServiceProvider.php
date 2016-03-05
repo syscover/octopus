716d7fd3961ad3dea07ecd974360499cb1f22aa7
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-05 11:47:08 +0100

    update service provider

--- src/Syscover/Octopus/OctopusServiceProvider.php
+++ src/Syscover/Octopus/OctopusServiceProvider.php
@@ -1,7 +1,6 @@
 <?php namespace Syscover\Octopus;
 
 use Illuminate\Support\ServiceProvider;
-use Syscover\Pulsar\Libraries\CustomValidator;
 
 class OctopusServiceProvider extends ServiceProvider
 {
@@ -13,23 +12,24 @@ class OctopusServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		// include route.php file
-		include realpath(__DIR__ . '/../../routes.php');
+		if (!$this->app->routesAreCached())
+			require __DIR__ . '/../../routes.php';
 
 		// register views
-		$this->loadViewsFrom(realpath(__DIR__ . '/../../views'), 'octopus');
+		$this->loadViewsFrom(__DIR__ . '/../../views', 'octopus');
 
         // register translations
-        $this->loadTranslationsFrom(realpath(__DIR__ . '/../../lang'), 'octopus');
+        $this->loadTranslationsFrom(__DIR__ . '/../../lang', 'octopus');
 
         // register migrations
         $this->publishes([
-            __DIR__.'/../../database/migrations/' => base_path('/database/migrations'),
-			__DIR__.'/../../database/migrations/updates/' => base_path('/database/migrations/updates'),
+            __DIR__ . '/../../database/migrations/' 		=> base_path('/database/migrations'),
+			__DIR__ . '/../../database/migrations/updates/' => base_path('/database/migrations/updates'),
         ], 'migrations');
 
         // register migrations
         $this->publishes([
-            __DIR__.'/../../database/seeds/' => base_path('/database/seeds')
+            __DIR__ . '/../../database/seeds/' 				=> base_path('/database/seeds')
         ], 'seeds');
 	}
 
@@ -42,5 +42,4 @@ class OctopusServiceProvider extends ServiceProvider
 	{
         //
 	}
-
 }
