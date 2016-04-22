68cfc4cfb69f6b8bc31bdc699638fad7669ed207
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-22 19:16:55 +0200

    create order from email

--- src/views/includes/block/actions_request_notification.blade.php
+++ src/views/includes/block/actions_request_notification.blade.php
@@ -2,7 +2,9 @@
     <td style='background:#ffffff;text-align:left;vertical-align:top;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;color:#000000;padding:0;border-spacing:0;border-collapse:collapse'>
         <p class="small grey" style='font-family:"Helvetica Neue",helvetica,arial,sans-serif;color:grey;font-size:12px;line-height:15px;margin:0 0 15px'>
             {{ trans('pulsar::pulsar.actions_email') }},
-            <a href="{{ route('editOctopusSupervisorRequest', ['id' => $octopusRequest->id_078, 'offset' => 0]) }}">{{ trans('octopus::pulsar.view_request') }}</a>
+            <a href="{{ route('editOctopusRequest', ['id' => $octopusRequest->id_078, 'offset' => 0]) }}">{{ trans('octopus::pulsar.view_request') }}</a>
+            |
+            <a href="{{ route('createOctopusOrder', ['id' => $octopusRequest->id_078, 'offset' => 0]) }}">{{ trans('octopus::pulsar.create_order') }}</a>
         </p>
     </td>
 </tr>
