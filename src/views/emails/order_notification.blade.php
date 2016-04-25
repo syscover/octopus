79d114943b20a85c6eb5b7e99244bf8e8ad4d16d
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-25 18:34:55 +0200

    improve emails communications

--- src/views/emails/order_notification.blade.php
+++ src/views/emails/order_notification.blade.php
@@ -10,6 +10,7 @@
     <tr>
         <td colspan="2" width="100%">&nbsp;</td>
     </tr>
+    <!-- supervisor -->
     <tr>
         <td colspan="2" class="header_body brown" valign="middle" width="100%" style='background:#ffffff;text-align:left;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;border-collapse:collapse;padding:0;border-spacing:0;vertical-align:middle;padding-left:10px;width:auto !important'>
             <strong style="color:brown;">{{ trans_choice('octopus::pulsar.supervisor', 1) }}</strong>
@@ -23,6 +24,8 @@
             <br>
         </td>
     </tr>
+    <!-- /.supervisor -->
+    <!-- shop -->
     <tr>
         <td colspan="2" class="header_body brown" valign="middle" width="100%" style='background:#ffffff;text-align:left;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;border-collapse:collapse;padding:0;border-spacing:0;vertical-align:middle;padding-left:10px;width:auto !important'>
             <strong style="color:brown;">{{ trans_choice('octopus::pulsar.shop', 1) }}</strong>
@@ -43,17 +46,21 @@
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans_choice('pulsar::pulsar.country', 1) }}:</strong> {{ $shop->name_002 }}
             </div>
-            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
-                <strong>{{ $order->territorial_area_2_002 }}:</strong> {{ $shop->name_004 }}
-            </div>
+            @if(! empty($shop->territorial_area_2_002))
+                <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                    <strong>{{ $shop->territorial_area_2_002 }}:</strong> {{ $shop->name_004 }}
+                </div>
+            @endif
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.cp') }}:</strong> {{ $shop->cp_076 }}
             </div>
         </td>
         <td class="content_body" style='background:#ffffff;text-align:left;vertical-align:top;font-size:15px;line-height:19px;border-collapse:collapse;color:#000000;border-spacing:0;font-family:"Helvetica Neue",helvetica,arial,sans-serif;padding:0 0 0 55px'>
-            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
-                <strong>{{ $order->territorial_area_1_002 }}:</strong> {{ $shop->name_003 }}
-            </div>
+            @if(! empty($shop->name_003))
+                <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                    <strong>{{ $shop->territorial_area_1_002 }}:</strong> {{ $shop->name_003 }}
+                </div>
+            @endif
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.locality') }}:</strong> {{ $shop->locality_076 }}
             </div>
@@ -83,8 +90,8 @@
             <br>
         </td>
     </tr>
-
-
+    <!-- /.shop -->
+    <!-- address -->
     <tr>
         <td colspan="2" class="header_body brown" valign="middle" width="100%" style='background:#ffffff;text-align:left;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;border-collapse:collapse;padding:0;border-spacing:0;vertical-align:middle;padding-left:10px;width:auto !important'>
             <strong style="color:brown;">{{ trans_choice('pulsar::pulsar.address', 1) }}</strong>
@@ -98,9 +105,11 @@
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans_choice('pulsar::pulsar.country', 1) }}:</strong> {{ $order->name_002 }}
             </div>
-            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
-                <strong>{{ $order->territorial_area_2_002 }}:</strong> {{ $order->name_004 }}
-            </div>
+            @if(! empty($order->name_004))
+                <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                    <strong>{{ $order->territorial_area_2_002 }}:</strong> {{ $order->name_004 }}
+                </div>
+            @endif
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.cp') }}:</strong> {{ $order->cp_079 }}
             </div>
@@ -110,9 +119,11 @@
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.name') }}:</strong> {{ $order->name_079 . ' ' . $order->surname_079  }}
             </div>
-            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
-                <strong>{{ $order->territorial_area_1_002 }}:</strong> {{ $order->name_003 }}
-            </div>
+            @if(! empty($order->name_003))
+                <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                    <strong>{{ $order->territorial_area_1_002 }}:</strong> {{ $order->name_003 }}
+                </div>
+            @endif
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.locality') }}:</strong> {{ $order->locality_079 }}
             </div>
@@ -142,11 +153,18 @@
             <br>
         </td>
     </tr>
-
-
+    <tr>
+        <td colspan="2" class="content_body" style='background:#ffffff;text-align:left;vertical-align:top;font-size:15px;line-height:19px;border-collapse:collapse;color:#000000;border-spacing:0;font-family:"Helvetica Neue",helvetica,arial,sans-serif;padding:0 0 0 55px'>
+            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                <strong>{{ trans('pulsar::pulsar.observations') }}:</strong> {!! $shop->observations_079 !!}
+            </div>
+        </td>
+    </tr>
+    <!-- /.address -->
+    <!-- order -->
     <tr>
         <td colspan="2" class="header_body brown" valign="middle" width="100%" style='background:#ffffff;text-align:left;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;border-collapse:collapse;padding:0;border-spacing:0;vertical-align:middle;padding-left:10px;width:auto !important'>
-            <strong style="color:brown;">{{ trans_choice('octopus::pulsar.request', 1) }}</strong>
+            <strong style="color:brown;">{{ trans_choice('octopus::pulsar.order', 1) }}</strong>
         </td>
     </tr>
     <tr>
@@ -172,6 +190,9 @@
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.units') }}:</strong> {{ $order->units_079 }}
             </div>
+            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                <strong>{{ trans_choice('pulsar::pulsar.comment', 2) }}:</strong> {!! $order->comments_079 !!}
+            </div>
         </td>
 
         <td class="content_body" style='background:#ffffff;text-align:left;vertical-align:top;font-size:15px;line-height:19px;border-collapse:collapse;color:#000000;border-spacing:0;font-family:"Helvetica Neue",helvetica,arial,sans-serif;padding:0 0 0 55px'>
@@ -195,6 +216,7 @@
             </div>
         </td>
     </tr>
+    <!-- /.order -->
     <tr>
         <td colspan="2" width="100%">&nbsp;</td>
     </tr>
--- src/views/emails/request_notification.blade.php
+++ src/views/emails/request_notification.blade.php
@@ -10,6 +10,7 @@
     <tr>
         <td colspan="2" width="100%">&nbsp;</td>
     </tr>
+    <!-- supervisor -->
     <tr>
         <td colspan="2" class="header_body brown" valign="middle" width="100%" style='background:#ffffff;text-align:left;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;border-collapse:collapse;padding:0;border-spacing:0;vertical-align:middle;padding-left:10px;width:auto !important'>
             <strong style="color:brown;">{{ trans_choice('octopus::pulsar.supervisor', 1) }}</strong>
@@ -23,6 +24,8 @@
             <br>
         </td>
     </tr>
+    <!-- /.supervisor -->
+    <!-- shop -->
     <tr>
         <td colspan="2" class="header_body brown" valign="middle" width="100%" style='background:#ffffff;text-align:left;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;border-collapse:collapse;padding:0;border-spacing:0;vertical-align:middle;padding-left:10px;width:auto !important'>
             <strong style="color:brown;">{{ trans_choice('octopus::pulsar.shop', 1) }}</strong>
@@ -43,17 +46,21 @@
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans_choice('pulsar::pulsar.country', 1) }}:</strong> {{ $shop->name_002 }}
             </div>
-            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
-                <strong>{{ $octopusRequest->territorial_area_2_002 }}:</strong> {{ $shop->name_004 }}
-            </div>
+            @if(! empty($shop->name_004))
+                <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                    <strong>{{ $shop->territorial_area_2_002 }}:</strong> {{ $shop->name_004 }}
+                </div>
+            @endif
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.cp') }}:</strong> {{ $shop->cp_076 }}
             </div>
         </td>
         <td class="content_body" style='background:#ffffff;text-align:left;vertical-align:top;font-size:15px;line-height:19px;border-collapse:collapse;color:#000000;border-spacing:0;font-family:"Helvetica Neue",helvetica,arial,sans-serif;padding:0 0 0 55px'>
-            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
-                <strong>{{ $octopusRequest->territorial_area_1_002 }}:</strong> {{ $shop->name_003 }}
-            </div>
+            @if(! empty($shop->name_003))
+                <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                    <strong>{{ $shop->territorial_area_1_002 }}:</strong> {{ $shop->name_003 }}
+                </div>
+            @endif
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.locality') }}:</strong> {{ $shop->locality_076 }}
             </div>
@@ -83,8 +90,8 @@
             <br>
         </td>
     </tr>
-
-
+    <!-- /.shop -->
+    <!-- address -->
     <tr>
         <td colspan="2" class="header_body brown" valign="middle" width="100%" style='background:#ffffff;text-align:left;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;border-collapse:collapse;padding:0;border-spacing:0;vertical-align:middle;padding-left:10px;width:auto !important'>
             <strong style="color:brown;">{{ trans_choice('pulsar::pulsar.address', 1) }}</strong>
@@ -98,9 +105,11 @@
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans_choice('pulsar::pulsar.country', 1) }}:</strong> {{ $octopusRequest->name_002 }}
             </div>
-            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
-                <strong>{{ $octopusRequest->territorial_area_2_002 }}:</strong> {{ $octopusRequest->name_004 }}
-            </div>
+            @if(! empty($octopusRequest->name_004))
+                <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                    <strong>{{ $octopusRequest->territorial_area_2_002 }}:</strong> {{ $octopusRequest->name_004 }}
+                </div>
+            @endif
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.cp') }}:</strong> {{ $octopusRequest->cp_078 }}
             </div>
@@ -110,9 +119,11 @@
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.name') }}:</strong> {{ $octopusRequest->name_078 . ' ' . $octopusRequest->surname_078  }}
             </div>
-            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
-                <strong>{{ $octopusRequest->territorial_area_1_002 }}:</strong> {{ $octopusRequest->name_003 }}
-            </div>
+            @if(! empty($octopusRequest->name_003))
+                <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                    <strong>{{ $octopusRequest->territorial_area_1_002 }}:</strong> {{ $octopusRequest->name_003 }}
+                </div>
+            @endif
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.locality') }}:</strong> {{ $octopusRequest->locality_078 }}
             </div>
@@ -142,8 +153,15 @@
             <br>
         </td>
     </tr>
-
-
+    <tr>
+        <td colspan="2" class="content_body" style='background:#ffffff;text-align:left;vertical-align:top;font-size:15px;line-height:19px;border-collapse:collapse;color:#000000;border-spacing:0;font-family:"Helvetica Neue",helvetica,arial,sans-serif;padding:0 0 0 55px'>
+            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                <strong>{{ trans('pulsar::pulsar.observations') }}:</strong> {!! $octopusRequest->observations_078 !!}
+            </div>
+        </td>
+    </tr>
+    <!-- /.address -->
+    <!-- request -->
     <tr>
         <td colspan="2" class="header_body brown" valign="middle" width="100%" style='background:#ffffff;text-align:left;font-size:15px;line-height:19px;font-family:"Helvetica Neue",helvetica,arial,sans-serif;border-collapse:collapse;padding:0;border-spacing:0;vertical-align:middle;padding-left:10px;width:auto !important'>
             <strong style="color:brown;">{{ trans_choice('octopus::pulsar.request', 1) }}</strong>
@@ -172,6 +190,9 @@
             <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
                 <strong>{{ trans('pulsar::pulsar.units') }}:</strong> {{ $octopusRequest->units_078 }}
             </div>
+            <div class="formatted_content" style="padding-bottom:19px;padding:0 !important;border:none !important;margin:0 0 5px !important;max-width:none !important">
+                <strong>{{ trans_choice('pulsar::pulsar.comment', 2) }}:</strong> {!! $octopusRequest->comments_078 !!}
+            </div>
         </td>
 
         <td class="content_body" style='background:#ffffff;text-align:left;vertical-align:top;font-size:15px;line-height:19px;border-collapse:collapse;color:#000000;border-spacing:0;font-family:"Helvetica Neue",helvetica,arial,sans-serif;padding:0 0 0 55px'>
@@ -195,6 +216,7 @@
             </div>
         </td>
     </tr>
+    <!-- /.request -->
     <tr>
         <td colspan="2" width="100%">&nbsp;</td>
     </tr>
